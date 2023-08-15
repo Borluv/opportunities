b759825a707f545ecaec7c93ee6f167720843f05
rpire <rubenpire7@gmail.com>
2023-08-15 15:17:14 -0400

    Install and configure ESLint
    Install and configure ESLint as a static code analyzer for TypeScript.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import App from './App.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
