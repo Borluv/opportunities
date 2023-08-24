48d3a409b9cfb3235f233c1be0ee5a22b5082562
rpire <rubenpire7@gmail.com>
2023-08-24 14:33:07 -0400

    Implement the sortProperties function in api/assets.ts

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,4 +1,5 @@
 import type { Property } from '../shared/types/common';
+import sortProperties from '../shared/logic/sortProperties';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL.concat('assets/');
 const headers = { Accept: 'application/json', 'Content-Type': 'application/json' };
@@ -6,7 +7,10 @@ const headers = { Accept: 'application/json', 'Content-Type': 'application/json'
 export const fetchAssets = async (): Promise<Property[]> =>
   await fetch(baseURL, { method: 'GET', headers })
     .then((response) => response.json())
-    .then((data) => data);
+    .then((data) => {
+      sortProperties(data);
+      return data;
+    });
 
 export const pickAsset = async (assetId: string | undefined): Promise<Property> => {
   if (!assetId) throw new Error('Missing params');
