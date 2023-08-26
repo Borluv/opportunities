cd578f3f5d73d9de162ddad92cbfe8b8bb532c8e
rpire <rubenpire7@gmail.com>
2023-08-26 00:24:05 -0400

    Replace the assets/properties file with assets/areas

---
+++ src/assets/areas.ts
@@ -0,0 +1,102 @@
+export default [
+  {
+    id: '581-w-27th-st',
+    area: '2000 rentable sqft',
+  },
+  {
+    id: '2821-w-25th-pi',
+    area: '14,362 rentable sqft',
+  },
+  {
+    id: '4801-s-congress-ave',
+    area: '35,654 rentable sqft',
+  },
+  {
+    id: '5999-s-university-dr',
+    area: '41434 sqft',
+  },
+  {
+    id: '6950-s-congress-ave',
+    area: '11,200 sqft',
+  },
+  {
+    id: '11520-sw-88th-st',
+    area: '44,259 rentable sqft',
+  },
+  {
+    id: '13508-n-florida-ave',
+    area: '63,370 rentable sqft',
+  },
+  {
+    id: '178-nw-44th-st',
+    area: '7,200 sqft',
+  },
+  {
+    id: '179-nw-44th-st',
+    area: '7,200 sqft',
+  },
+  {
+    id: '181-nw-44th-st',
+    area: '1,500 rentable sqft',
+  },
+  {
+    id: '191-nw-44th-st',
+    area: '7,200 sqft',
+  },
+  {
+    id: '3922-nw-2nd-ave',
+    area: '1,776 rentable sqft',
+  },
+  {
+    id: '3930-nw-2nd-ave',
+    area: '1,500 rentable sqft',
+  },
+  {
+    id: '4200-nw-2nd-ave',
+    area: '1,075 rentable sqft',
+  },
+  {
+    id: '4202-nw-2nd-ave',
+    area: '1,325 rentable sqft',
+  },
+  {
+    id: '4204-nw-2nd-ave',
+    area: '4,200 rentable sqft',
+  },
+  {
+    id: '4314-nw-2nd-ave',
+    area: '5,483 rentable sqft',
+  },
+  {
+    id: '4401-nw-2nd-ave',
+    area: '1,000 rentable sqft',
+  },
+  {
+    id: '4431-nw-2nd-ave',
+    area: '800 rentable sqft',
+  },
+  {
+    id: '4433-nw-2nd-ave',
+    area: '2,800 rentable sqft',
+  },
+  {
+    id: '4744-nw-2nd-ave',
+    area: '5,550 rentable sqft',
+  },
+  {
+    id: '4795-nw-2nd-ave',
+    area: '10,944 sqft',
+  },
+  {
+    id: '4810-nw-2nd-ave',
+    area: '9,990 sqft',
+  },
+  {
+    id: '4840-nw-2nd-ave',
+    area: '4,773 sqft',
+  },
+  {
+    id: '5001-nw-2nd-ave',
+    area: '4,773 sqft',
+  },
+];
