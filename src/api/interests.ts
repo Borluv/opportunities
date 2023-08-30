e0d69a20f1b9bf5282668ccb9e56a0a7f3f94fa7
rpire <rubenpire7@gmail.com>
2023-08-30 11:35:58 -0400

    Create the fetchLeadInterests API call

--- src/api/interests.ts
+++ src/api/interests.ts
@@ -1,7 +1,19 @@
+import type { Interest } from '../shared/types/common';
+
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 const appHeaders = { Accept: 'application/json', 'Content-Type': 'application/json' };
 
-export const fetchInterests = (): string => "I'll leave it like this for now...";
+export const fetchLeadInterests = async (leadId: string): Promise<Interest[]> => {
+  const Authorization = localStorage.getItem('auth') || '';
+  const headers = { ...appHeaders, Authorization };
+
+  return await fetch(`${baseURL}leads/${leadId}/interests`, { method: 'GET', headers })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) throw new Error(data.error);
+      return data;
+    });
+};
 
 export const createInterest = async (leadId: string, assetId: string): Promise<void> => {
   await fetch(`${baseURL}leads/${leadId}/interests`, {
